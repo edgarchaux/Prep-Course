b3b36f1308de3804db4d8d511747869db75eefee
Édgar Chaux <edgareduardo.@gmail.com>
2022-07-05 20:22:31 -0500

    .

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -199,15 +199,15 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
-  return nombre + ' ' +apellido; 
+  var combinacion=nombre + ' ' +apellido;
+  return combinacion; 
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return 'Hola ' + nombre;
+  return 'Hola ' + nombre + '!';
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
@@ -246,29 +246,16 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if(letra=='a'){
-    return 'Es vocal';
-  }
-  if(letra=='e'){
-    return 'Es vocal';
-  }
-  if(letra=='i'){
-    return 'Es vocal';
-  }
-  if(letra=='o'){
-    return 'Es vocal';
-  }
-  if(letra=='u'){
-    return 'Es vocal';
-  }
-  else {
-    return 'Dato incorrecto';
-  }
   if(letra.length>1){
     return 'Dato incorrecto';
+    }
+    else if(letra==='a'||letra==='e'||letra==='i'||letra==='o'||letra==='u'){
+    return 'Es vocal';
+    }
+    else {
+    return 'Dato incorrecto';
+    }
   }
-  
-}
 
 
 
