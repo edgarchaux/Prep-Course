6dca59ab1bf7188ad152ab31e24f3610881ffd32
Édgar Chaux <edgareduardo.@gmail.com>
2022-07-27 18:11:46 -0500

    .

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -181,10 +181,10 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  var i=0
+  var i=0;
   while(i<=10){
-    console.log(i*6)
-    i++
+    return(i*6);
+    i++;
   }
 }
 
