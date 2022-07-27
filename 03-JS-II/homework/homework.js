1b30376943eda6c063c6e9f13dbb81d4c9bef59d
Édgar Chaux <edgareduardo.@gmail.com>
2022-07-27 11:50:42 -0500

    z

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -150,20 +150,21 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  if (numero>=-1||<=1){
+  if (numero>-1&&numero<1){
     return false;
-  } else{
-    for(i>1; i < numero; i++){
+  } else if (numero>1||numero<-1) {
+    for(var i=2; i < numero; i++){
       if  ((numero/i)%1===0){
         return false;
-      }else{
+      } else{
         return true;
       }
     }
   }
+}
 
 
-
+  
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
