7781c001c3605d40f9358e5eb7fcf37b5645314c
Édgar Chaux <edgareduardo.@gmail.com>
2022-07-26 19:55:55 -0500

    .

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -86,11 +86,7 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (numero===10||numero===5){
-    return 'true';
-  } else{
-    return 'false';
-  }
+  return (numero===10||numero===5);
 }
 
 function estaEnRango(numero) {
