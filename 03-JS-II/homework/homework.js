8d6ef464681daebca01dea24f13a07d301ee99ac
Édgar Chaux <edgareduardo.@gmail.com>
2022-07-26 19:44:20 -0500

    .

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -68,17 +68,17 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
-  switch(color){
-    case 'blue':
-      console.log ('This is blue');
-    case 'red':
-      console.log ('This is red');
-    case 'green':
-      console.log ('This is green');
-    case 'orange':
-      console.log ('This is orange');
-    default:
-      console.log ('Color not found');
+  switch(color) {
+    case "blue":
+      return "This is blue";
+    case "red": 
+      return "This is red";
+    case "green":
+      return "This is green";
+    case "orange":
+      return "This is orange";
+    default: 
+    return "Color not found";
   }
 }
 
